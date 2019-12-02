8be476e32cb155353d422b1a2a8fdd9dc0dca612
NightSnack <chiyexiao@icloud.com>
2019-12-02 13:40:55 -0500

    project first commit

---
+++ resnet10/Residual.py
@@ -0,0 +1,29 @@
+import d2lzh as d2l
+from mxnet import autograd, gluon, init
+from mxnet.gluon import data as gdata, loss as gloss, nn
+import os
+import pandas as pd
+import shutil
+import time
+
+class Residual(nn.HybridBlock):
+    def __init__(self, num_channels, use_1x1conv=False, strides=1, **kwargs):
+        super(Residual, self).__init__(**kwargs)
+        self.conv1 = nn.Conv2D(num_channels, kernel_size=3, padding=1,
+                               strides=strides)
+        self.conv2 = nn.Conv2D(num_channels, kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2D(num_channels, kernel_size=1,
+                                   strides=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm()
+        self.bn2 = nn.BatchNorm()
+
+    def hybrid_forward(self, F, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        return F.relu(Y + X)
+
